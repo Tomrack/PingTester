5639bab12cd2047db8857c5eb016d945d57c4773
Tomrack <thomas.rack@aol.com>
2015-06-15 15:35:31 +0100

    R.I.P Old code

---
+++ Ping.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(156, 101);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(47, 13);
+            this.timeLabel.TabIndex = 1;
+            this.timeLabel.Text = "seconds";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(265, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Type URL/IP:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(184, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.listView1.Location = new System.Drawing.Point(41, 122);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(193, 218);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(358, 243);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(119, 97);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(36, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(7, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(170, 50);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 460);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(308, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "This website and its content is copyright of Cambridge Software ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 472);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "All rights reserved. © Cambridge Software 2015. ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(135, 483);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(153, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "www.cambridgesoftware.co.uk";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ping every";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 519);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.timeLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(465, 558);
+            this.MinimumSize = new System.Drawing.Size(465, 558);
+            this.Name = "Form1";
+            this.Text = "Ping";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Ping.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        private void pingSender()
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            // Use the default Ttl value which is 128, 
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted. 
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            try
+            {
+
+
+
+
+
+                string[] words = textBox1.Text.Split(',');
+                int i;
+                for (i = 0; i < words.GetLength(0); i++)
+                {
+                    PingReply reply = pingSender.Send(words[i].Trim(), timeout, buffer, options);
+                    if (reply.Status == IPStatus.Success)
+
+                    {
+
+                        string[] arr = new string[4];
+                        ListViewItem itm;
+
+                        //Add first item
+                        arr[0] = words[i].Trim();
+                        arr[1] = reply.Status.ToString();
+                        arr[2] = reply.RoundtripTime.ToString() + "ms";
+                        itm = new ListViewItem(arr);
+                        listView1.Items.Add(itm);
+                        itm.ForeColor = System.Drawing.Color.Green;
+
+                        // MessageBox.Show("Complete."  +
+                        /* Environment.NewLine +
+                             "Ping recieved: " + reply.Address.ToString() +
+                         Environment.NewLine +
+                         "Size of data: " + reply.Buffer.Length.ToString() + "bytes" +
+                         Environment.NewLine +
+                         "How long the connection lasted: " + reply.Options.Ttl.ToString() + "ms" +
+                         Environment.NewLine +
+                         "Ping: " + reply.RoundtripTime.ToString(), "Success", MessageBoxButtons.OKCancel);*/
+
+
+                    }
+                    else
+                    {
+                        string[] arr = new string[4];
+                        ListViewItem itm;
+                        //Add first item
+                        arr[0] = words[i].Trim();
+                        arr[1] = reply.Status.ToString();
+                        arr[2] = "N/A";
+                        itm = new ListViewItem(arr);
+                        listView1.Items.Add(itm);
+                        itm.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+
+                //type - name - value
+                object sender = this;
+                EventArgs e = new EventArgs();
+
+                button2_Click(sender, e);
+                MessageBox.Show("Ping Not Recieved.; " + ex.Message, "Error!", MessageBoxButtons.OKCancel);
+
+                
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int timeLeft { get; private set; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pingSender();
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            if (timeLeft > 0)
+            {
+
+
+                // Display the new time left 
+                // by updating the Time Left label.
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " seconds";
+
+                int value = (int)numericUpDown1.Value * 1;
+
+                if (value > 0) { timeLeft = value;
+                }
+     
+
+                else
+                {
+
+                    object sender2 = this;
+                    EventArgs e2 = new EventArgs();
+
+                    button2_Click(sender2, e2);
+                    MessageBox.Show("value Cannot Be 0", "Error!", MessageBoxButtons.OKCancel);
+                }
+
+               
+               
+            }
+
+            else
+            {
+
+                int value = (int)numericUpDown1.Value;
+                timeLeft = value;
+                pingSender();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Add column header
+            listView1.Columns.Add("Ping recieved by:", 150);
+            listView1.Columns.Add("Status:", 90);
+            listView1.Columns.Add("Ping:", 65);
+            listView1.MinimumSize = new System.Drawing.Size(300, 300);
+
+
+
+
+            timeLeft = 0;
+        }
+
+
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty) return;
+            timer1.Start(); textBox1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop(); textBox1.Enabled = true;
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timeLabel_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.Width = 270;
+            listView1.FullRowSelect = true;
+
+
+            //Add column header
+            listView1.Columns.Add("ProductName", 100);
+            listView1.Columns.Add("Price", 70);
+            listView1.Columns.Add("Quantity", 70);
+
+            //Add items in the listview
+          
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://www.cambridgesoftware.co.uk"); 
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://www.cambridgesoftware.co.uk"); 
+        }
+    }
+}
+
+
+
+            /*Add first item
+            arr[0] = "product_1";
+            arr[1] = "100";
+            arr[2] = "10";
+            itm = new ListViewItem(arr);
+            listView1.Items.Add(itm);
+
+            //Add second item
+            arr[0] = "product_2";
+            arr[1] = "200";
+            arr[2] = "20";
+            itm = new ListViewItem(arr);
+            listView1.Items.Add(itm);
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            string productName = null;
+            string price = null;
+            string quantity = null;
+            
+            productName = listView1.SelectedItems[0].SubItems[0].Text;
+            price = listView1.SelectedItems[0].SubItems[1].Text;
+            quantity = listView1.SelectedItems[0].SubItems[2].Text;
+
+            MessageBox.Show(productName + " , " + price + " , " + quantity);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+        }*/
+    
+    
+
+
+  
+
+
